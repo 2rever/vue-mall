c68c6bebd0280c96da2fff5aab8aa03b7709d178
xieshian <xieshian@fujiacaifu.com>
2019-01-31 12:19:17 +0800

    '1.更改api接口，因为easy-mock挂了'

--- src/components/pages/shoppingMall.vue
+++ src/components/pages/shoppingMall.vue
@@ -3,7 +3,7 @@
     <div class="search-bar">
       <van-row class="test-row">
         <van-col span="3">
-          <van-icon name="location" color="#fff" style="padding-left:0.8rem;"/>
+          <van-icon name="location" color="#fff" style="padding-left:0.8rem;" />
         </van-col>
         <van-col span="16">
           <input type="text" class="search-input">
@@ -72,112 +72,127 @@
 </template>
 
 <script>
-import axios from "axios";
-import floorComponent from "../component/floorComponent";
-import goodsInfoComponent from "../component/goodsInfoComponent";
-import { toMoney } from "@/filter/moneyFilter.js";
-import url from '@/serviceAPI.config.js'
-export default {
-  filters: {
-    moneyFilter(money) {
-      return toMoney(money);
+  import axios from "axios";
+  import floorComponent from "../component/floorComponent";
+  import goodsInfoComponent from "../component/goodsInfoComponent";
+  import {
+    toMoney
+  } from "@/filter/moneyFilter.js";
+  import url from '@/serviceAPI.config.js'
+  export default {
+    filters: {
+      moneyFilter(money) {
+        return toMoney(money);
+      }
+    },
+    components: {
+      floorComponent,
+      goodsInfoComponent
+    },
+    data() {
+      return {
+        swiperOption: {
+          slidesPerView: 3
+        },
+        msg: "Shopping Mall",
+        bannerPic: [],
+        category: [],
+        adBanner: "",
+        recommendGoods: [],
+        floor1: [],
+        floor2: [],
+        floor3: [],
+        floorName: {},
+        hotGoods: []
+      };
+    },
+    created() {
+      axios
+        .get(
+          'http://reachxout.coding.me/miao/mallInfo.json'
+        )
+        .then(res => {
+          console.log(res.data.data);
+          if (res.status == 200) {
+            this.category = res.data.data.category;
+            this.adBanner = res.data.data.advertesPicture.PICTURE_ADDRESS;
+            this.bannerPic = res.data.data.slides;
+            this.recommendGoods = res.data.data.recommend;
+            this.floor1 = res.data.data.floor1;
+            this.floor2 = res.data.data.floor2;
+            this.floor3 = res.data.data.floor3;
+            this.floorName = res.data.data.floorName;
+            this.hotGoods = res.data.data.hotGoods;
+          }
+        })
+        .catch(err => console.log(err));
     }
-  },
-  components: { floorComponent, goodsInfoComponent },
-  data() {
-    return {
-      swiperOption: {
-        slidesPerView: 3
-      },
-      msg: "Shopping Mall",
-      bannerPic: [],
-      category: [],
-      adBanner: "",
-      recommendGoods: [],
-      floor1: [],
-      floor2: [],
-      floor3: [],
-      floorName: {},
-      hotGoods: []
-    };
-  },
-  created() {
-    axios
-      .get(
-        url.getShopingMallInfo
-      )
-      .then(res => {
-        console.log(res.data.data);
-        if (res.status == 200) {
-          this.category = res.data.data.category;
-          this.adBanner = res.data.data.advertesPicture.PICTURE_ADDRESS;
-          this.bannerPic = res.data.data.slides;
-          this.recommendGoods = res.data.data.recommend;
-          this.floor1 = res.data.data.floor1;
-          this.floor2 = res.data.data.floor2;
-          this.floor3 = res.data.data.floor3;
-          this.floorName = res.data.data.floorName;
-          this.hotGoods = res.data.data.hotGoods;
-        }
-      })
-      .catch(err => console.log(err));
-  }
-};
+  };
+
 </script>
 
 <style scoped>
-.search-bar {
-  height: 2.2rem;
-  background-color: #e5017d;
-  line-height: 2.2rem;
-}
-.search-input {
-  width: 100%;
-  height: 1.3rem;
-  border-top: 0px;
-  border-left: 0px;
-  border-right: 0px;
-  border-bottom: 1px solid #fff !important;
-  background-color: #e5017d;
-  color: #fff;
-}
-.type-bar {
-  background-color: #fff;
-  margin: 0 0.3rem 0.3rem 0.3rem;
-  border-radius: 0.3rem;
-  font-size: 14px;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-}
-.type-bar div {
-  padding: 0.3rem;
-  font-size: 12px;
-  text-align: center;
-}
-.recommend-area {
-  background-color: #fff;
-  margin-top: 0.3rem;
-}
-.recommend-title {
-  border-bottom: 1px solid #eee;
-  font-size: 14px;
-  padding: 0.2rem;
-  color: #e5017d;
-}
-.recommend-body {
-  border-bottom: 1px solid #eee;
-}
-.recommend-item {
-  width: 99%;
-  border-right: 1px solid #eee;
-  font-size: 12px;
-  text-align: center;
-}
-.hot-area {
-  text-align: center;
-  font-size: 14px;
-  height: 1.8rem;
-  line-height: 1.8rem;
-}
+  .search-bar {
+    height: 2.2rem;
+    background-color: #e5017d;
+    line-height: 2.2rem;
+  }
+
+  .search-input {
+    width: 100%;
+    height: 1.3rem;
+    border-top: 0px;
+    border-left: 0px;
+    border-right: 0px;
+    border-bottom: 1px solid #fff !important;
+    background-color: #e5017d;
+    color: #fff;
+  }
+
+  .type-bar {
+    background-color: #fff;
+    margin: 0 0.3rem 0.3rem 0.3rem;
+    border-radius: 0.3rem;
+    font-size: 14px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+  }
+
+  .type-bar div {
+    padding: 0.3rem;
+    font-size: 12px;
+    text-align: center;
+  }
+
+  .recommend-area {
+    background-color: #fff;
+    margin-top: 0.3rem;
+  }
+
+  .recommend-title {
+    border-bottom: 1px solid #eee;
+    font-size: 14px;
+    padding: 0.2rem;
+    color: #e5017d;
+  }
+
+  .recommend-body {
+    border-bottom: 1px solid #eee;
+  }
+
+  .recommend-item {
+    width: 99%;
+    border-right: 1px solid #eee;
+    font-size: 12px;
+    text-align: center;
+  }
+
+  .hot-area {
+    text-align: center;
+    font-size: 14px;
+    height: 1.8rem;
+    line-height: 1.8rem;
+  }
+
 </style>
