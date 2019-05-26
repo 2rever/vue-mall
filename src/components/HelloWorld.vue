a2f94275d5e14ac5a8bd24819b7937f99e7c39ec
shaun <805087550@qq.com>
2019-05-26 20:10:11 +0800

    1.修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
-    <van-button type="danger" >超级按钮</van-button>
+    <van-button type="danger" >超级按钮ss</van-button>
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
